d76dd77545643fc6d0607d3482d84f013edc2c0d
Sergey Melentyev <sergey.melentyev@gmail.com>
2015-12-28 12:26:24 +0300

    new version

--- FirstStart/FirstStart/main.m
+++ FirstStart/FirstStart/main.m
@@ -8,10 +8,40 @@
 
 #import <Foundation/Foundation.h>
 
+// defining a structure with type definition
+typedef struct {
+    float x;
+    float y;
+} MyPoint;
+
+typedef struct {
+    MyPoint startPoint;
+    MyPoint endPoint;
+} MyLine;
+
+// defining an enum of type NSUInteger with three values
+typedef enum MyEnum : NSUInteger {
+    MyFirstValue,
+    MySecondValue,
+    MyThirdValue
+} MyEnum;
+
 int main(int argc, const char * argv[]) {
+    
+    MyPoint pointA;   // variable "pointA" of type myPoint
+    pointA.x = 10.1;    // assigning a value to a struct
+    pointA.y = 12.1;
+    
+    MyPoint pointB;
+    pointB.x = 30.1;
+    pointB.y = 29.1;
+    
+    MyLine line;
+    line.startPoint = pointA;
+    line.endPoint = pointB;
+    
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        NSLog(@"%f, %f", pointA.x, pointA.y);   // format specifiers, values
     }
     return 0;
 }
