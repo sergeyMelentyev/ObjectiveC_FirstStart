d54d25922a03e6505ba2bfc3000bbbede1c120ac
Sergey Melentyev <sergey.melentyev@gmail.com>
2015-12-28 11:11:07 +0300

    new version

---
+++ FirstStart/FirstStart/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  FirstStart
+//
+//  Created by Melentyev on 28.12.15.
+//  Copyright © 2015 Melentyev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
