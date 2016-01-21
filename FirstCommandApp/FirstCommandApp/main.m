9f854ddbfebab0313c2d6b0b5d5b0ff2fa4ede90
Sergey Melentyev <sergey.melentyev@gmail.com>
2016-01-21 11:46:28 +0300

    new version

---
+++ FirstCommandApp/FirstCommandApp/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  FirstCommandApp
+//
+//  Created by Melentyev on 21.01.16.
+//  Copyright © 2016 Melentyev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
