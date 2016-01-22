71191c3a4f4da3a1dc62b81f98b3584add52aa16
Sergey Melentyev <sergey.melentyev@gmail.com>
2016-01-22 14:05:08 +0300

    new version

---
+++ FirstCommandApp/FirstCommandApp/Mammal.h
@@ -0,0 +1,20 @@
+//
+//  Mammal.h
+//  FirstCommandApp
+//
+//  Created by Melentyev on 22.01.16.
+//  Copyright © 2016 Melentyev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <Cocoa/Cocoa.h>
+
+// Declare a "Mammal" class that inherits from top level NSObject class
+@interface Mammal : NSObject
+// Interface contains properties and methods
+
+
+
+
+
+@end
---
+++ FirstCommandApp/FirstCommandApp/Mammal.m
@@ -0,0 +1,13 @@
+//
+//  Mammal.m
+//  FirstCommandApp
+//
+//  Created by Админ on 22.01.16.
+//  Copyright © 2016 Melentyev. All rights reserved.
+//
+
+#import "Mammal.h"
+
+@implementation Mammal
+
+@end
